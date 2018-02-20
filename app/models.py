5348a6ecd3700413e8c89669be7c2e872f7d7e37
roger <rogerokello@gmail.com>
2018-02-20 14:02:31 +0300

    Created a business class with an init method

--- app/models.py
+++ app/models.py
@@ -1,1 +1,9 @@
 from .db import businesses
+
+class Business(): 
+    #initialise business   
+    def __init__(self, name, category, location):
+        """Initialize the business with an name, category and location."""
+        self.name = name
+        self.category = category
+        self.location = location
