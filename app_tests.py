f090fc0810f7a163404a37aca6a2b900713f5fdc
roger <rogerokello@gmail.com>
2018-02-20 14:16:13 +0300

    Created WeConnectApiTest case with setUp and tearDown methods

--- app_tests.py
+++ app_tests.py
@@ -1,3 +1,14 @@
 import unittest, json
 from flask import request
 from app import create_app, db
+
+class WeConnectApiTestCase(unittest.TestCase):
+    
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
