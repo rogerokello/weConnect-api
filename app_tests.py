ab7e01d4e6cb9652964424eea843aaa007d85af8
roger <rogerokello@gmail.com>
2018-02-20 14:20:37 +0300

    Added initialization to the setUp method

--- app_tests.py
+++ app_tests.py
@@ -3,9 +3,22 @@ from flask import request
 from app import create_app, db
 
 class WeConnectApiTestCase(unittest.TestCase):
-    
+
     def setUp(self):
-        pass
+        #create app using the flask import
+        self.app = create_app()
+
+        #create a test client
+        self.client = self.app.test_client
+
+        #create a dict to be used to add a new biz
+        self.a_business = {'name':'Xedrox',
+                            'category': 'IT',
+                            'location' : 'Lira'
+                            }
+        #bind the app context
+        with self.app.app_context():
+            pass
 
     def tearDown(self):
         pass
