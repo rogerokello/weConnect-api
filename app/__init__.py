71e8196a61ec8321226a6e25220222d080282ebc
roger <rogerokello@gmail.com>
2018-02-20 15:09:05 +0300

    Added functionality for registering a new business

--- app/__init__.py
+++ app/__init__.py
@@ -6,4 +6,30 @@ def create_app():
     from .models import Business
     from flask import request, jsonify, make_response
 
+    #Route to register a new business
+    @app.route('/businesses', methods=['POST'])
+    def register_a_business():
+        #tranform json data got into a dictionary
+        data = request.get_json()
+
+        #extract data from each of the dictionary
+        #values
+        name = data['name']
+        category = data['category']
+        location = data['location']
+
+        #create a business object
+        a_business = Business(name=name,
+                                category=category,
+                                location=location)
+
+        #add business to the non-persistent database
+        Business.add(a_business)
+
+        message = "Created business: " + a_business.name + "successfuly"
+        response = {
+            'message': message
+        }
+        return make_response(jsonify(response)), 201
+
     return app
