be99e1a170df72e3214a9abe0c80048c7357291d
roger <rogerokello@gmail.com>
2018-02-21 12:04:38 +0300

    Added defn 4 api resource to register a new user

--- app/auth/views.py
+++ app/auth/views.py
@@ -6,3 +6,6 @@ from app.models import User
 
 class RegistrationView(MethodView):
     """This class registers a new user."""
+
+# Define the API resource
+registration_view = RegistrationView.as_view('registration_view')
