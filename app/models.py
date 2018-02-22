3260912629092c1ccef30b28e5db079260001a90
roger <rogerokello@gmail.com>
2018-02-22 22:08:18 +0300

    Added User class method to generate token

--- app/models.py
+++ app/models.py
@@ -147,3 +147,25 @@ class User():
     #logout user and change login status
     def logout_user(self):
         self.logged_in = False
+
+    def generate_token(self, user_id):
+        """ Generates the access token"""
+
+        try:
+            # set up a payload with an expiration time
+            payload = {
+                'exp': datetime.utcnow() + timedelta(minutes=20),
+                'iat': datetime.utcnow(),
+                'sub': user_id
+            }
+            # create the byte string token using the payload and the SECRET key
+            jwt_string = jwt.encode(
+                payload,
+                current_app.config.get('SECRET_KEY'),
+                algorithm='HS256'
+            )
+            return jwt_string
+
+        except Exception as e:
+            # return an error in string format if an exception occurs
+            return str(e)
