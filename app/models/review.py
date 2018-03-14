6cecd782abc1996b9b11f34830330661aff17a2f
Roger Okello <rogerokello@gmail.com>
2018-03-14 12:12:36 +0300

    Removed index & unique constraint of Review model

--- app/models/review.py
+++ app/models/review.py
@@ -6,7 +6,7 @@ import jwt
 class Review(db.Model):
     __tablename__ = 'reviews'
     id = db.Column(db.Integer, primary_key=True)
-    review_summary = db.Column(db.String(64), index=True, unique=True)
+    review_summary = db.Column(db.String(64))
     review_description = db.Column(db.String(150))
     star_rating = db.Column(db.String(10))
     business_id = db.Column(db.Integer, db.ForeignKey('businesses.id'), nullable=False)
