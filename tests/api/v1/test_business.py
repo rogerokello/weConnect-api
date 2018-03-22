58b8cbeef952d358b939949c2a89f5e00b9205a4
Roger Okello <rogerokello@gmail.com>
2018-03-22 12:34:11 +0300

    Added test to filter out businesses by category

--- tests/api/v1/test_business.py
+++ tests/api/v1/test_business.py
@@ -661,3 +661,47 @@ class BusinessTestCase(unittest.TestCase):
         #check that a 201 response status code was returned
         self.assertEqual(response.status_code, 201)  
 
+    def test_api_can_filter_businesses_using_their_categories(self):
+        "Test that the api can fiter businesses using their categories"
+        """Test the API can get all business reviews (GET request)"""
+        # register a test user, then log them in
+        self.register_user()
+        result = self.login_user()
+
+        # obtain the access token
+        access_token = json.loads(result.data.decode())['access_token']
+
+        #first create a business in the IT category
+        self.client().post('/businesses',
+                            headers=dict(Authorization="Bearer " + access_token),
+                            data=json.dumps(
+                                {
+                                    'name':'Xedrox',
+                                    'category': 'IT',
+                                    'location' : 'Lira'
+                                }
+                            ),
+                            content_type='application/json')
+        
+        #Create another business in the Construction category
+        self.client().post('/businesses',
+                            headers=dict(Authorization="Bearer " + access_token),
+                            data=json.dumps(
+                                {
+                                    'name':'Megatrends',
+                                    'category': 'Construction',
+                                    'location' : 'Lira'
+                                }
+                            ),
+                            content_type='application/json')
+
+        #filter business using the category
+        response = self.client().get('/businesses/filter?categoryorlocation=construction',
+                            headers=dict(Authorization="Bearer " + access_token),
+                            content_type='application/json')
+
+        # check that Construction string in returned json response
+        self.assertIn('Construction', str(response.data)) 
+
+        #check that a 201 response status code was returned
+        self.assertEqual(response.status_code, 201)
