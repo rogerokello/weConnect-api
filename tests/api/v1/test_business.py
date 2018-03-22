e9b98284de0cd44826bbbb9796aa7b9445523d2b
Roger Okello <rogerokello@gmail.com>
2018-03-22 16:35:52 +0300

    Added test to return a limited number of businesses

--- tests/api/v1/test_business.py
+++ tests/api/v1/test_business.py
@@ -747,3 +747,72 @@ class BusinessTestCase(unittest.TestCase):
 
         #check that a 201 response status code was returned
         self.assertEqual(response.status_code, 201)
+
+    def test_api_can_return_a_no_of_businesses_specified_by_user_using_get_param_limit(self):
+        "Test that the api can return a specified number of businesses"
+        # register a test user, then log them in
+        self.register_user()
+        result = self.login_user()
+
+        # obtain the access token
+        access_token = json.loads(result.data.decode())['access_token']
+
+        #first create a business in the IT category
+        self.client().post('/businesses',
+                            headers=dict(Authorization="Bearer " + access_token),
+                            data=json.dumps(
+                                {
+                                    'name':'Xedrox',
+                                    'category': 'IT',
+                                    'location' : 'Lira'
+                                }
+                            ),
+                            content_type='application/json')
+        
+        #Create another business located in Kampala
+        self.client().post('/businesses',
+                            headers=dict(Authorization="Bearer " + access_token),
+                            data=json.dumps(
+                                {
+                                    'name':'Megatrends',
+                                    'category': 'Construction',
+                                    'location' : 'Kampala'
+                                }
+                            ),
+                            content_type='application/json')
+
+        #Create another business located in the US
+        self.client().post('/businesses',
+                            headers=dict(Authorization="Bearer " + access_token),
+                            data=json.dumps(
+                                {
+                                    'name':'Microsoft',
+                                    'category': 'IT',
+                                    'location' : 'United States of America'
+                                }
+                            ),
+                            content_type='application/json')
+
+        #Create another business located in Ireland
+        self.client().post('/businesses',
+                            headers=dict(Authorization="Bearer " + access_token),
+                            data=json.dumps(
+                                {
+                                    'name':'LinkedIn',
+                                    'category': 'IT',
+                                    'location' : 'Ireland Republic'
+                                }
+                            ),
+                            content_type='application/json')
+
+        #Return 2 businesses
+        response = self.client().get('/businesses/paginate?limit=2',
+                            headers=dict(Authorization="Bearer " + access_token),
+                            content_type='application/json')
+
+        # check that only two businesses are returned
+        self.assertEqual(2, len(json.loads(response.data)["message"]))
+
+        #check that a 201 response status code was returned
+        self.assertEqual(response.status_code, 201)
+    
